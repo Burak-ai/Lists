9289aca5f92ce34f18684fe0b122433c2f5ebdde
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-07 10:11:57 +0300

    Add files via upload

---
+++ List.py
@@ -0,0 +1,34 @@
+planets = ['Mercur', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets[1])
+print(planets[2:5])
+print(planets[-1])
+planets[3] = 'Malacandra'
+print(planets)
+print(len(planets))
+
+"""
+# Sorted The planets in alphabetical order
+sorted(planets)"""
+
+primes = [2, 3, 5, 7]
+print(sum(primes))
+print(max(primes))
+
+# list.append modifies a list by adding an item to the end:
+planets.append("Pluto")
+print(planets)
+
+# list.pop removes and returns the last element of a list:
+planets.pop()
+print(planets)
+
+
+# Where does Earth fall in the order of planets?
+planet_index = planets.index('Earth') # Search list
+print(planet_index)
+
+print('Earth' in planets)
+print('Calbefraques' in planets)
+
+
+
